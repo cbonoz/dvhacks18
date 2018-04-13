a8854f66f933ce2c6565a99de344a0de7ef409f0
cbonoz <chrisdistrict@gmail.com>
2018-04-13 16:37:23 -0700

    update test harness

--- server/routable_test.js
+++ server/routable_test.js
@@ -1,3 +1,21 @@
 /**
  * Created by cbuonocore on 4/13/18.
  */
+// Tests for the routable.js library.
+const assert = require("assert"); // node.js core testing module.
+const routable = require('./routable');
+
+describe('routable', function () {
+
+    it('is ok', function () {
+        assert.ok(true);
+    });
+
+
+});
+
+
+
+
+
+
