d782e006cac17867f82fe66316793047379dd2a6
cbonoz <chrisdistrict@gmail.com>
2018-04-13 18:31:37 -0700

    add tsp/vrp test

--- server/routable.js
+++ server/routable.js
@@ -1,3 +1,67 @@
 /**
  * Created by cbuonocore on 4/13/18.
  */
+const library = (function () {
+
+    const ortools = require('node_or_tools');
+
+    const getRandom = (items) => {
+        return items[Math.floor(Math.random() * items.length)];
+    };
+
+    const formatDateTimeMs = (timeMs) => {
+        const date = new Date(parseInt(timeMs));
+        return `${date.toDateString()} ${date.toLocaleTimeString()}`;
+    };
+
+    function capitalize(str) {
+        if (str) {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+        return str;
+    }
+
+    /**
+     * Multi-vehicle node visit optimization
+     * @param vrpSolverOpts options for solving port vrp.
+     * @param vrpSearchOpts options for searching port vrp solution.
+     * @param cb: (err, solution) => { ... }
+     */
+    function solveVRP(vrpSolverOpts, vrpSearchOpts, cb) {
+        const VRP = new ortools.VRP(vrpSolverOpts);
+        VRP.Solve(vrpSearchOpts, cb);
+    }
+
+    /**
+     * Single-vehicle node visit optimization
+     * @param tspSolverOpts options for solving port tsp.
+     * @param tspSearchOpts options for searching port tsp.
+     * @param cb: (err, solution) => { ... }
+     */
+    function solveTSP(tspSolverOpts, tspSearchOpts, cb) {
+        const TSP = new ortools.TSP(tspSolverOpts);
+        TSP.Solve(tspSearchOpts, cb);
+    }
+
+    function getCostMatrix(locations, costFunction) {
+        const costMatrix = new Array(locations.length);
+        for (let from = 0; from < locations.length; ++from) {
+            costMatrix[from] = new Array(locations.length);
+            for (let to = 0; to < locations.length; ++to) {
+                costMatrix[from][to] = costFunction(locations[from], locations[to]);
+            }
+        }
+    }
+
+    return {
+        capitalize: capitalize,
+        getRandom: getRandom,
+        getCostMatrix: getCostMatrix,
+        formatDateTimeMs: formatDateTimeMs,
+        solveVRP: solveVRP,
+        solveTSP: solveTSP
+    };
+
+})();
+module.exports = library;
+
--- server/server.js
+++ server/server.js
@@ -1,118 +1,57 @@
 // Author: Chris Buonocore (April 2018)
+// Project: Routable
 // License: MIT
 // Made for DV Hacks 2018
 
 (function () {
     "use strict";
-
     const axios = require('axios');
     const express = require('express');
     const cors = require('cors');
     const bodyParser = require('body-parser');
     const fs = require('fs');
     const http = require('http');
-// https://github.com/expressjs/multer
-    const multer = require('multer');
-    const upload = multer({dest: 'uploads/'});
-
-// const https = require('https');
-// const pg = require('pg');
+    const pg = require('pg');
 
     const PORT = 9001;
 
     const app = express();
     const server = http.createServer(app);
-// const io = require('socket.io')(server, {origins: '*:*'});
+    // const io = require('socket.io')(server, {origins: '*:*'});
 
-    const neon = require('./neon');
-    const rekeyed = require('./rekeyed');
-
-// neon.saveFileMetadata(args....).then()
+    const routable = require('./routable');
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-
 
     app.use(cors());
 
     app.get('/api/hello', (req, res) => {
         return res.json("hello world");
-    })
+    });
 
-// Return a list of files associated with the given address.
-    app.get('/api/files/:address', (req, res) => {
-        const address = req.params.address;
+    /**
+     * Returns the optimal schedule for the given driver and day.
+     * Returned as a list of ordered nodes.
+     * day: date in format MM-dd-YYYY
+     * driverId: driver id of the desired user
+     */
+    app.get('/api/schedule/:day/:driverId', (req, res) => {
+        const day = req.params.day;
         console.log('address');
 
-// TODO [NEO]: make request to blockchain to retrieve all file metadatas associated with this address.
         const files = [];
-
-
         return res.json(files);
-    })
-    /** Permissible loading a single file,
-     the value of the attribute "name" in the form of "recfile". **/
-    const type = upload.any();
-
-
-    app.post('/api/authorize', type, function (req, res, next) {
-        // req.body contains the text fields
-        const privateKey = req.body.privateKey;
-        const publicKey = req.body.publicKey;
-        const targetAddress = req.body.address;
-
-
-        const fileName = address + "_" + name;
-
-        // Save the encrypted file to the upload directory, and return success.
-        rekeyed.encryptAndSaveFile(fileContent, fileName, key, function (err, results) {
-
-            if (err) {
-                console.error('error', err);
-                return res.status(500).json(err);
-            }
-
-            // TODO [NEO]: Save the metdata for the current file to the Neo blockchain after saved here.
-
-
-            console.log('results', results);
-            return res.json(results);
-        });
     });
 
-
-    app.post('/api/upload', type, function (req, res, next) {
-        // req.body contains the text fields
-        const fileContent = req.body.file;
-        const metadata = JSON.parse(req.body.metadata);
-        // TODO: save these metadata fields to Neo.
-        const name = metadata.name;
-        const lastModified = metadata.lastModifiedDate;
-        const address = metadata.address;
-        const fileHash = metadata.hash;
-        const key = metadata.key; // TODO: confirm signing authority.
-        console.log(fileContent);
-
-        const fileName = address + "_" + name;
-
-        // Save the encrypted file to the upload directory, and return success.
-        rekeyed.encryptAndSaveFile(fileContent, fileName, key, function (err, results) {
-
-            if (err) {
-                console.error('error', err);
-                return res.status(500).json(err);
-            }
-
-            // TODO [NEO]: Save the metdata for the current file to the Neo blockchain after saved here.
-
-
-            console.log('results', results);
-            return res.json(results);
-        });
-    });
-
-
-    // TODO: add route for retrieving decrypted file if sufficient permissions.
+    // app.post('/api/authorize', type, function (req, res, next) {
+    //     // req.body contains the text fields
+    // });
+    //
+    // app.post('/api/upload', type, function (req, res, next) {
+    //     // req.body contains the text fields
+    //     const fileContent = req.body.file;
+    // });
 
     server.listen(PORT, () => {
         console.log('Express server listening on localhost port: ' + PORT);
